7197ddf30e53408a9102e3362215d9e388ff108b
Jose Aguayo <jpaguayo11@yahoo.es>
2018-05-03 19:29:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,7 @@ var playlist = {coldplay: "Fix You"}
 function updatePlaylist(playlist, artistName, songTitle) {
   return playlist[artistName] = songTitle
 }
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+}
